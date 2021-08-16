48b25bb45d614cfcd329138930d901de7f43abf4
Brian Harrison <brianharrison.2002@gmail.com>
2021-08-16 14:07:41 -0400

    app error

--- albums/views.py
+++ albums/views.py
@@ -9,12 +9,5 @@ def list_albums(request):
     return render(request, "albums/list_albums.html",
                   {"albums": Album})
 
-# def show_contact(request, pk):
-#     contact = get_object_or_404(Contact, pk=pk)
-#     form = NoteForm()
-#     return render(
-#         request,
-#         "contacts/show_contact.html",
-#         {"contact": contact, "pk": pk, "form": form},
-#     )
+
 
